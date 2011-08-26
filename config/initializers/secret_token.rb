19175ece800a2cddbc70ee8a18d81e4d812861f5
metameme <metameme@maniaphobic.org>
2011-08-26 16:18:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odometers::Application.config.secret_token = 'aa0bd9c41b0eff4903e75221b442fc9aa358f4274541411c94ba024ee4ca2d8414996f5e1d940cd91a5118629ac1549d254d4dd9351667cb8ce54bdb7d9454a1'
